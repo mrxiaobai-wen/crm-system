90825958b50acdbabdda8589152354ac4a9eed63
wendiyou <1129307781@qq.com>
2020-12-01 19:28:55 +0800

    feat: 订单服务提交；

---
+++ crm-order/src/main/java/com/crm/order/CrmOrderApplication.java
@@ -0,0 +1,13 @@
+package com.crm.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrmOrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrmOrderApplication.class, args);
+    }
+
+}
